d1f18d7a4b3a182e87a9afdd2063348326dba5cb
Younes Moukhlij <youmoukh@e2r9p13.1337.ma>
2024-03-06 12:06:50 +0100

    new struct

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 15:10:21 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/03/05 18:34:19 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/03/06 12:06:09 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,22 @@
 #include <unistd.h>
 #include <readline/readline.h>
 
-
-
-typedef struct s_mini
+typedef struct s_minishell
 {
-	char			**command;
-	int				flag_input_output;
-	char			**after_out_in_put;
-	char			**env;
-	struct s_mini	*next;
-} t_mini;
+	char				**cmd;
+	char				*after_cmd;
+	int					flag_input_output;
+	char				*after_out_or_input;
+	char				**env;
+	struct s_minishell	*next;
+} t_minishell;
+
+
+
+
+
+
+
 
 // TOOLS
 int		ft_strlen(char *s);
@@ -39,14 +45,7 @@ int		ft_strcmp(char *s1, char *s2);
 // Funtions
 void	functions(t_mini *mini);
 
-
 t_mini	*return_data(void);
 t_mini	*return_data1(void);
 
 #endif
-
-if (tokken == ">") red
-if (tokken == "<") ro
-if (tokken == "<<") roa
-if (tokken == ">>")
-if (tokken == "|")
